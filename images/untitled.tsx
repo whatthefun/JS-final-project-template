62502b6f8e9fdb637ca7173f0ec3b0d0a097aa4d
whatthefun <tn9550803@gmail.com>
2017-07-29 17:53:14 +0800

    地圖
    新增一個測試地圖。

---
+++ images/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="material_1" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../resources/material_1.png" width="512" height="384"/>
+</tileset>
